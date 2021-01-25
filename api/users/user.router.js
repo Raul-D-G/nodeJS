ff0441b7346da7020c30031eef5ee1713f09d009
Gioancă Raul <raul-dumitru.gioanca@my.fmi.unibuc.ro>
2021-01-25 21:53:28 +0200

    verificare token

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -1,11 +1,13 @@
 const { createUser, getUsers, getUserByUserId, updateUser, deleteUser, login } = require("./user.controller");
 const router = require('express').Router();
+const { checkToken } = require("../../auth/token_validation")
 
-router.post("/", createUser);
-router.get("/", getUsers);
-router.get("/:id", getUserByUserId);
-router.patch("/", updateUser);
-router.delete("/", deleteUser);
+
+router.post("/", checkToken, createUser);
+router.get("/", checkToken, getUsers);
+router.get("/:id", checkToken, getUserByUserId);
+router.patch("/", checkToken, updateUser);
+router.delete("/", checkToken, deleteUser);
 
 router.post("/login", login);
 
---
+++ auth/token_validation.js
@@ -0,0 +1,26 @@
+const { verify } = require("jsonwebtoken");
+
+
+module.exports = {
+    checkToken: (req, res, next) => {
+        let token = req.get("authorization");
+        if (token) {
+            token = token.slice(7);
+            verify(token, process.env.QWE, (err, decoded) => {
+                if (err) {
+                    res.json({
+                        success: 0,
+                        message: "Acesul nu este permis! Token invalid"
+                    });
+                } else {
+                    next();
+                }
+            })
+        } else {
+            res.json({
+                success: 0,
+                message: "Acesul nu este permis! Token invalid"
+            });
+        }
+    }
+}
